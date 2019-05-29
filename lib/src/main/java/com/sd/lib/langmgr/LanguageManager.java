1845fd65f86d34c92611cc05685bcbd873e4e298
zjun <565061763@qq.com>
2019-05-29 15:35:26 +0800

    first commit

---
+++ lib/src/main/java/com/sd/lib/langmgr/LanguageManager.java
@@ -0,0 +1,128 @@
+package com.sd.lib.langmgr;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class LanguageManager
+{
+    private static final Map<Locale, LanguageModel> MAP_LOCALE_LANGUAGE = new HashMap<>();
+    private static final Map<LanguageModel, String> MAP_LANGUAGE = new LinkedHashMap<>();
+
+    private static final LanguageManager INSTANCE = new LanguageManager();
+    private LanguageModel mDefaultLanguageModel;
+
+    private LanguageManager()
+    {
+        // 简体中文
+        register(Locale.SIMPLIFIED_CHINESE, LanguageModel.SIMPLIFIED_CHINESE);
+        register(Locale.CHINESE, LanguageModel.SIMPLIFIED_CHINESE);
+
+        // 繁体中文
+        register(Locale.TRADITIONAL_CHINESE, LanguageModel.TRADITIONAL_CHINESE);
+        register(new Locale("zh", "HK"), LanguageModel.TRADITIONAL_CHINESE);
+
+        // 英文
+        register(Locale.ENGLISH, LanguageModel.ENGLISH);
+        register(Locale.US, LanguageModel.ENGLISH);
+        register(Locale.UK, LanguageModel.ENGLISH);
+        register(Locale.CANADA, LanguageModel.ENGLISH);
+    }
+
+    public static LanguageManager getInstance()
+    {
+        return INSTANCE;
+    }
+
+    /**
+     * 返回默认的语言处理对象
+     * <p>
+     * 第一次调用{@link #register(Locale, LanguageModel)}方法的时候绑定的对象会被当做默认的语言处理对象
+     *
+     * @return
+     */
+    public LanguageModel getDefaultLanguageModel()
+    {
+        if (mDefaultLanguageModel == null)
+            throw new RuntimeException("You must invoke register() method before this");
+
+        return mDefaultLanguageModel;
+    }
+
+    /**
+     * 注册
+     * <p>
+     * 第一次调用此方法的时候绑定的对象会被当做默认的语言处理对象
+     *
+     * @param locale        目标语言
+     * @param languageModel 目标语言的处理对象
+     */
+    public void register(Locale locale, LanguageModel languageModel)
+    {
+        if (locale == null || languageModel == null)
+            throw new IllegalArgumentException("local or languageModel is null");
+
+        MAP_LOCALE_LANGUAGE.put(locale, languageModel);
+        MAP_LANGUAGE.put(languageModel, null);
+
+        if (mDefaultLanguageModel == null)
+            mDefaultLanguageModel = languageModel;
+    }
+
+    /**
+     * 情况所有注册的语言
+     */
+    public void clearRegister()
+    {
+        MAP_LOCALE_LANGUAGE.clear();
+        MAP_LANGUAGE.clear();
+        mDefaultLanguageModel = null;
+    }
+
+    /**
+     * 返回目标语言对应的处理对象
+     *
+     * @param locale 目标语言
+     * @return
+     */
+    public LanguageModel getLanguageModel(Locale locale)
+    {
+        if (locale == null)
+            throw new IllegalArgumentException("local is null");
+
+        LanguageModel languageModel = MAP_LOCALE_LANGUAGE.get(locale);
+        if (languageModel == null)
+            languageModel = getDefaultLanguageModel();
+
+        return languageModel;
+    }
+
+    /**
+     * 是否支持某个语言
+     *
+     * @param model
+     * @return
+     */
+    public boolean containsLanguage(LanguageModel model)
+    {
+        return MAP_LANGUAGE.containsKey(model);
+    }
+
+    /**
+     * 返回所有已注册的{@link LanguageModel}
+     *
+     * @return
+     */
+    public List<LanguageModel> getAllLanguageModel()
+    {
+        final List<LanguageModel> list = new ArrayList<>(MAP_LANGUAGE.size());
+        for (Map.Entry<LanguageModel, String> item : MAP_LANGUAGE.entrySet())
+        {
+            list.add(item.getKey());
+        }
+        return list;
+    }
+}
---
+++ lib/src/main/java/com/sd/lib/langmgr/LanguageModel.java
@@ -0,0 +1,240 @@
+package com.sd.lib.langmgr;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Locale;
+
+public class LanguageModel
+{
+    /**
+     * 简体中文，对应{@link Locale#SIMPLIFIED_CHINESE}
+     */
+    public static final LanguageModel SIMPLIFIED_CHINESE = new LanguageModel(Locale.SIMPLIFIED_CHINESE, "简体中文");
+    /**
+     * 繁体中文，对应{@link Locale#TRADITIONAL_CHINESE}
+     */
+    public static final LanguageModel TRADITIONAL_CHINESE = new LanguageModel(Locale.TRADITIONAL_CHINESE, "繁體中文");
+    /**
+     * 英文，对应{@link Locale#ENGLISH}
+     */
+    public static final LanguageModel ENGLISH = new LanguageModel(Locale.ENGLISH, "English");
+
+    private static final String PERSISTENT_KEY = LanguageModel.class.getName();
+
+    private final String mLanguage;
+    private final String mCountry;
+
+    private final String mName;
+
+    public LanguageModel(Locale locale, String name)
+    {
+        this(locale.getLanguage(), locale.getCountry(), name);
+    }
+
+    private LanguageModel(String language, String country, String name)
+    {
+        if (TextUtils.isEmpty(language) || TextUtils.isEmpty(name))
+            throw new IllegalArgumentException("language or name is empty");
+
+        if (country == null)
+            country = "";
+
+        mLanguage = language;
+        mCountry = country;
+        mName = name;
+    }
+
+    /**
+     * 返回语言名称
+     *
+     * @return
+     */
+    public String getName()
+    {
+        return mName;
+    }
+
+    /**
+     * 转{@link Locale}
+     *
+     * @return
+     */
+    public Locale toLocale()
+    {
+        return new Locale(mLanguage, mCountry);
+    }
+
+    /**
+     * 将Resources修改为当前语言
+     *
+     * @param resources
+     */
+    public void apply(Resources resources)
+    {
+        final Locale locale = toLocale();
+
+        final Configuration configuration = resources.getConfiguration();
+        if (Build.VERSION.SDK_INT >= 17)
+        {
+            configuration.setLocale(locale);
+        } else
+        {
+            configuration.locale = locale;
+        }
+
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof LanguageModel))
+            return false;
+
+        final LanguageModel other = (LanguageModel) obj;
+
+        return mLanguage.equals(other.mLanguage)
+                && mCountry.equals(other.mCountry)
+                && mName.equals(other.mName);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return (mLanguage + "_" + mCountry + "_" + mName).hashCode();
+    }
+
+    /**
+     * 返回当前App的语言
+     *
+     * @param context
+     * @return
+     */
+    public static LanguageModel getCurrent(Context context)
+    {
+        LanguageModel model = queryModel(context);
+        if (model == null)
+        {
+            final Locale locale = context.getResources().getConfiguration().locale;
+            model = LanguageManager.getInstance().getLanguageModel(locale);
+        } else
+        {
+            if (LanguageManager.getInstance().containsLanguage(model))
+            {
+                return model;
+            } else
+            {
+                // 保存的对象已经不在注册列表中了，清空保存的对象，并返回默认的处理对象
+                clearModel(context);
+                return LanguageManager.getInstance().getDefaultLanguageModel();
+            }
+        }
+
+        return model;
+    }
+
+    /**
+     * 设置当前App的语言对象
+     *
+     * @param model
+     * @param context
+     * @return
+     */
+    public static boolean setCurrent(Context context, LanguageModel model)
+    {
+        if (LanguageManager.getInstance().containsLanguage(model))
+        {
+            return saveModel(context, model);
+        } else
+        {
+            throw new IllegalArgumentException("LanguageModel is not registered: " + model);
+        }
+    }
+
+    /**
+     * 保存对象到本地
+     *
+     * @param context
+     * @param model
+     * @return
+     */
+    private static boolean saveModel(Context context, LanguageModel model)
+    {
+        if (model == null)
+            throw new IllegalArgumentException("LanguageModel is null when save to local");
+
+        final String saveString = toJson(model);
+        if (TextUtils.isEmpty(saveString))
+            return false;
+
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.edit().putString(PERSISTENT_KEY, saveString).commit();
+    }
+
+    /**
+     * 查询本地保存的对象
+     *
+     * @param context
+     * @return
+     */
+    private static LanguageModel queryModel(Context context)
+    {
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        final String json = sharedPreferences.getString(PERSISTENT_KEY, null);
+        return fromJson(json);
+    }
+
+    /**
+     * 清空本地保存的对象
+     *
+     * @param context
+     * @return
+     */
+    private static boolean clearModel(Context context)
+    {
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (sharedPreferences.contains(PERSISTENT_KEY))
+            return sharedPreferences.edit().remove(PERSISTENT_KEY).commit();
+
+        return false;
+    }
+
+    private static String toJson(LanguageModel model)
+    {
+        try
+        {
+            final JSONObject jsonObject = new JSONObject();
+            jsonObject.put("language", model.mLanguage);
+            jsonObject.put("country", model.mCountry);
+            jsonObject.put("name", model.mName);
+            return jsonObject.toString();
+        } catch (JSONException e)
+        {
+            return null;
+        }
+    }
+
+    private static LanguageModel fromJson(String json)
+    {
+        try
+        {
+            final JSONObject jsonObject = new JSONObject(json);
+            final String language = jsonObject.getString("language");
+            final String country = jsonObject.getString("country");
+            final String name = jsonObject.getString("name");
+            return new LanguageModel(language, country, name);
+        } catch (Exception e)
+        {
+            return null;
+        }
+    }
+}
