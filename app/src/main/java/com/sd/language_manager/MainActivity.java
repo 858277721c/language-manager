1ea6305634816a2113174bb006479b34d795bbb2
zjun <565061763@qq.com>
2019-05-29 15:40:22 +0800

    demo

--- app/src/main/java/com/sd/language_manager/MainActivity.java
+++ app/src/main/java/com/sd/language_manager/MainActivity.java
@@ -1,14 +1,49 @@
 package com.sd.language_manager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+import com.sd.lib.langmgr.LanguageModel;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        final LanguageModel model = LanguageModel.getCurrent(this);
+        model.apply(getResources());
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (LanguageModel.SIMPLIFIED_CHINESE.equals(model))
+            ((TextView) (findViewById(R.id.btn_simplified_chinese))).setTextColor(Color.RED);
+        else if (LanguageModel.TRADITIONAL_CHINESE.equals(model))
+            ((TextView) (findViewById(R.id.btn_traditional_chinese))).setTextColor(Color.RED);
+        else if (LanguageModel.ENGLISH.equals(model))
+            ((TextView) (findViewById(R.id.btn_english))).setTextColor(Color.RED);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.btn_simplified_chinese:
+                LanguageModel.setCurrent(this, LanguageModel.SIMPLIFIED_CHINESE);
+                recreate();
+                break;
+            case R.id.btn_traditional_chinese:
+                LanguageModel.setCurrent(this, LanguageModel.TRADITIONAL_CHINESE);
+                recreate();
+                break;
+            case R.id.btn_english:
+                LanguageModel.setCurrent(this, LanguageModel.ENGLISH);
+                recreate();
+                break;
+        }
     }
 }
